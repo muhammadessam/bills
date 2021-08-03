7755a105357c3fae478170116c385a271f33f9fb
Muhammad Essam <muhammadessamwork@outlook.com>
2021-08-03 13:50:58 +0200

    basic installation

---
+++ database/seeders/BouncerSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Silber\Bouncer\BouncerFacade as Bouncer;
+
+class BouncerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Bouncer::role()->firstOrCreate([
+            'name' => 'admin',
+            'title' => 'Administrator',
+        ]);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => \Hash::make('password'),
+        ])->assign('admin');
+    }
+}
